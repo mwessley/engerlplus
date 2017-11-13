cc406a8242a3934d504e94015c6f5b9a80880e70
matthias <matthias.wess@siemens.com>
2017-11-13 13:45:05 +0100

    optimize cycle

--- engerl.py
+++ engerl.py
@@ -1,15 +1,12 @@
 import numpy as np
 names = ('ma','vicky','mischa','jaqueline','martin','anna','matti','giulia','wolfi','maria')
 order = np.random.permutation(10)
-cont = (np.append(order,order[0])/2).astype(int)
 i=0
 while i<10:
-    if cont[i] != cont[i+1]:
+    if int(order[i]/2) != int(order[(i+1)%10]/2):
         i+=1
     else:
         order = np.random.permutation(10)
-        cont = (np.append(order,order[0])/2).astype(int)
         i=0
-order = np.append(order,order[0])
 for i in range(10):
-    print(names[order[i]] + ' -> ' + names[order[i+1]])
+    print(names[order[i]] + ' -> ' + names[order[(i+1)%10]])
